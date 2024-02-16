04464a29e600365e1109487e2e6340173a5672ef
Karenina Wongsordjo <karenina2798@gmail.com>
2024-02-16 16:52:52 +0900

    edit htmls

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -22,6 +22,6 @@ class BookmarksController < ApplicationController
 
   private
   def bookmarks_params
-    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
